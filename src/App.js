718daa9a674ff28a892c97dd8a1424b85cac447a
shivam123425 <shivam123425@gmail.com>
2019-08-14 21:39:35 +0530

    Displayed phone directory

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div>
+        <h1>Phone Directory App</h1>
+        <h2>Directory List</h2>
+        <button>Delete</button>
+      </div>
+    </Fragment>
   );
 }
 
 export default App;
-
-
